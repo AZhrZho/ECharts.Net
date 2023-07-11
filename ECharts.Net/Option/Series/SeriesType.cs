0161cddfaddb7425f79000ae2fb7336f38834166
AZhrZho <1137361788@qq.com>
2023-07-11 18:24:51 +0800

    feat: add series types denination

--- ECharts.Net/Option/Series/SeriesType.cs
+++ ECharts.Net/Option/Series/SeriesType.cs
@@ -3,4 +3,25 @@
 public enum SeriesType
 {
     Line,
+    Bar,
+    Pie,
+    Scatter,
+    EffectScatter,
+    Radar,
+    Tree,
+    Treemap,
+    Sunburst,
+    Boxplot,
+    Candlestick,
+    Heatmap,
+    Map,
+    Parallel,
+    Lines,
+    Graph,
+    Sankey,
+    Funnel,
+    Gauge,
+    PictorialBar,
+    ThemeRiver,
+    Custom
 }
