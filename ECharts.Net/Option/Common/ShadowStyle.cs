0d594dcd853a6fdbaaca68f857933e863465835a
AZhrZho <1137361788@qq.com>
2023-07-11 18:56:08 +0800

    feat: implement option.tooltip.axisPointer

---
+++ ECharts.Net/Option/Common/ShadowStyle.cs
@@ -0,0 +1,11 @@
+﻿namespace ECharts.Net;
+
+public class ShadowStyle
+{
+    public Color? Color { get; set; }
+    public double? ShadowBlur { get; set; }
+    public Color? ShadowColor { get; set; }
+    public double? ShadowOffsetX { get; set; }
+    public double? ShadowOffsetY { get; set; }
+    public double? Opacity { get; set; }
+}
