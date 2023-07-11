cfced4f4460cf90cd939927a2f23c11a3ced48ce
AZ <1137361788@qq.com>
2023-07-11 16:01:06 +0800

    feat: add basic support for option.Toolbox (#17)

--- ECharts.Net/Option/Option.cs
+++ ECharts.Net/Option/Option.cs
@@ -10,5 +10,6 @@ public class Option
     public XAxis? XAxis { get; set; }
     public YAxis? YAxis { get; set; }
     public Tooltip? Tooltip { get; set; }
+    public Toolbox? Toolbox { get; set; }
     public IList<Series>? Series { get; set; }
 }
---
+++ ECharts.Net/Option/Toolbox/Feature.cs
@@ -0,0 +1,12 @@
+﻿namespace ECharts.Net;
+
+// TODO: Implement Features
+public class Feature
+{
+    public object? SaveAsImage { get; set; }
+    public object? Restore { get; set; }
+    public object? DataView { get; set; }
+    public object? DataZoom { get; set; }
+    public object? MagicType { get; set; }
+    public object? Brush { get; set; }
+}
