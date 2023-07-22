28d3c89f8ca7a640308b31862c2e1aaf5a11468c
AZ <1137361788@qq.com>
2023-07-22 13:45:22 +0800

    feat: add Label, Dimension for LineSeries (#24)

---
+++ ECharts.Net/Option/Common/Dimension.cs
@@ -0,0 +1,22 @@
+﻿namespace ECharts.Net;
+
+public class Dimension
+{
+    public string? Name { get; set; }
+    public DimensionDataType? Type { get; set; }
+    public string? DisplayName { get; set; }
+
+    public static implicit operator Dimension(string name)
+    {
+        return new Dimension { Name = name };
+    }
+}
+
+public enum DimensionDataType
+{
+    Number,
+    Ordinal,
+    Float,
+    Int,
+    Time
+}
